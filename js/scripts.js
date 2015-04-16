892722eba6e377ce1f07af44518e725021c3e74c
Brett Moore & Bojana Skarich <student@epicodus.com>
2015-04-16 16:56:15 -0700

    Modify click handler.

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,47 @@
 $(document).ready(function() {
 
-  $("form#create-list").submit(function(event) {
-      event.preventDefault();
-
-    var inputtedList = $("input#create").val();
-
-    var newList = {
-      list: inputtedList,
-      tasks: []
-    };
-
-    $("ol#lists").append("<li><span class='listitem'>" + inputtedList + "</span></li>");
-
-    // $(".listitem").off("submit");
-    $(".listitem").last().click(function() {
-
-      $(".createtasks").show();
-      $("#taskform").show();
-
-      newList.tasks.forEach(function(task) {
-            $(".show-tasks").append("<li>" + task.description + "</li>");
-
-        });
-
-    $("form#tasks").off("submit");
-
-      $("form#tasks").submit(function(event) {
-        var inputtedTask = $("input#description").val();
-        var newTask = {
-          description: inputtedTask
-        };
-        newList.tasks.push(newTask);
-        $(".show-tasks").empty();
-        newList.tasks.forEach(function(task) {
-              $(".show-tasks").append("<li>" + task.description + "</li>");
-        });
+    $("form#create-list").submit(function(event) {
         event.preventDefault();
 
-      });
+        var inputtedList = $("input#create").val();
+
+        var newList = {
+            list: inputtedList,
+            tasks: []
+        };
+
+        $("ol#lists").append("<li><span class='listitem'>" + inputtedList + "</span></li>");
+
+        // $(".listitem").off("submit");
+        $(".listitem").last().click(function() {
+
+            $(".createtasks").show();
+            $("#taskform").show();
+            $(".list-name").text(newList.list);
+
+            newList.tasks.forEach(function(task) {
+                $(".show-tasks").append("<li>" + task.description + "</li>");
+
+            });
+
+            $("form#tasks").off("submit");
+
+            $("form#tasks").submit(function(event) {
+                var inputtedTask = $("input#description").val();
+                var newTask = {
+                    description: inputtedTask
+                };
+                newList.tasks.push(newTask);
+                $(".show-tasks").empty();
+                newList.tasks.forEach(function(task) {
+                    $(".show-tasks").append("<li>" + task.description + "</li>");
+                });
+                event.preventDefault();
+
+            });
+
+        });
 
     });
 
-  });
-
-  });
+});

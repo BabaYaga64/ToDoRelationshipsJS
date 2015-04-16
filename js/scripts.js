67b9596d34ffebae431577933222daef451c176b
Brett Moore & Bojana Skarich <student@epicodus.com>
2015-04-16 12:19:25 -0700

    Add initial files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
+    event.preventDefault();
+
+    $("form#create-lists").submit(function(event) {
+
+        var inputtedList = $("input.create").val("");
+        console.log(inputtedList);
+        $("ul#lists").append("<li><span class='listitem'>" + inputtedList + "</span></li>");
+    });
+
     $("form#tasks").submit(function(event) {
-        event.preventDefault();
 
-        var description = $("input#description").val();
+        var newList = { list: inputtedList, tasks: [] };
 
-        var newTask = { newDescription: description };
+        $("#all-tasks").each(function() {
+            var inputtedTask = $(this).find("input.new-task").val();
+
+            var newTask = { description: inputtedTask };
+            newList.tasks.push(newTask);
+
+        });
 
         $("ul#all-tasks").append("<li><span class='task'>" + newTask.newDescription + "</span></li>");
 
